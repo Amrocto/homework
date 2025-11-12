077ffd8769fff3afed2fae9b1b1c5366157a3c99
Нияз <niyaz@MacBook-Pro-Niaz.local>
2025-11-12 21:16:47 +0300

    Third homework

--- dice.js
+++
@@ -1,7 +0,0 @@
-function randomDice(dice){
-    if(!/^d\d+$/.test(dice)){
-        throw new Error('Неверный формат кубика, ожидается d4, d6, d8 и т.д.');
-    }
-    const max = Number(dice.slice(1));
-    return Math.ceil(Math.random() * max);
-}
