7dc3f91fe1daf1a6038f2bd5dd2192ac4cf7e050
Нияз <niyaz@MacBook-Pro-Niaz.local>
2026-01-12 09:44:25 +0300

    pokemon homework

---
+++ pokemon.js
@@ -0,0 +1,23 @@
+const url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', url);
+xhr.send();
+xhr.addEventListener('load', function(){
+    if(this.status >= 200 && this.status < 300) {
+        try{
+            const data = JSON.parse(this.response);
+            const newUrl = data.abilities[0].ability.url;
+            const xhr2 = new XMLHttpRequest();
+            xhr2.open('GET', newUrl);
+            xhr2.send();
+            xhr2.addEventListener('load', function(){
+                const data = JSON.parse(this.response);
+                console.log(data);
+            })
+        }
+        catch(err){
+            console.error('json parse failed occured', err);
+        }
+    } else console.error('nerwork failed occured', this.status);
+})
