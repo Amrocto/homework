c677f0fcdaf5455a2a077863f4638a23668377a4
Нияз <niyaz@MacBook-Pro-Niaz.local>
2026-01-15 10:17:57 +0300

    promise pokemon

---
+++ pokemon-promise.js
@@ -0,0 +1,39 @@
+// const url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+
+// const xhr = new XMLHttpRequest();
+// xhr.open('GET', url);
+// xhr.send();
+// xhr.addEventListener('load', function(){
+//     if(this.status >= 200 && this.status < 300) {
+//         try{
+//             const data = JSON.parse(this.response);
+//             const newUrl = data.abilities[0].ability.url;
+//             const xhr2 = new XMLHttpRequest();
+//             xhr2.open('GET', newUrl);
+//             xhr2.send();
+//             xhr2.addEventListener('load', function(){
+//                 const data = JSON.parse(this.response);
+//                 const englishEntry = data.effect_entries.find(entry => entry.language.name === 'en'); 
+//                 console.log(englishEntry.effect);
+//             })
+//         }
+//         catch(err){
+//             console.error('json parse failed occured', err);
+//         }
+//     } else console.error('nerwork failed occured', this.status);
+// })
+
+const url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+function getAbility(url){
+    return fetch(url)
+    .then(response => {
+        if(!response.ok) throw new Error('HTTP error');
+        else return response.json()})
+    .then(data => fetch(data.abilities[0].ability.url))
+    .then(response => {
+        if(!response.ok) throw new Error('inner HTTP error');
+        else return response.json()
+    })
+    .then(data => data.effect_entries.find(entry => entry.language.name === 'en').effect);
+}
+getAbility(url).then(response => console.log(response).catch(err => console.error(err.message)));
