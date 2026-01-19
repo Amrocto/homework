303f5075cb5326717b47e27efca72dea2edee01c
Нияз <niyaz@MacBook-Pro-Niaz.local>
2026-01-19 08:41:19 +0300

    geo saved

--- geo.js
+++ geo.js
@@ -1,7 +1,24 @@
-// navigator.geolocation.getCurrentPosition((position) => {
-//     doSomething(position.coords.latitude, position.coords.longitude)
-// });
+p1 = new Promise((resolve,reject) => {
+    function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+        reject(err);
+      }
+      function success(position){
+        const crd = position.coords;
+        console.log('Your current position is:');
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`More or less ${crd.accuracy} meters.`);
+        resolve(crd);
+      }
+    navigator.geolocation.getCurrentPosition(success, error)
+});
 
-navigator.geolocation.getCurrentPosition(position => {
-    const { latitude, longitude } = position.coords
-  })
+p1.then(response => console.log(response)).catch(err => console.log(err));
+
+
+// navigator.geolocation.getCurrentPosition(position => {
+//     const { latitude, longitude } = position.coords
+//   })
+
+
